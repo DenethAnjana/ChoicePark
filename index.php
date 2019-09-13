11cb5013036650bf428211f1658d647442f1826c
H.A Deneth Anjana Perera <denethanjana95@gmail.com>
2019-09-13 20:31:49 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,117 +6,54 @@ Author: H.A.D.A. Perera
 <!DOCTYPE html>
 <html>
 <head>
-                <?php include './_top_header.php'; ?>             
-</head>
-	
+                <?php include './_top_header.php'; ?>  
+
+
+</head>	
 <body>
     
     
-<!-- header -->
-	<?php include './_header.php'; ?>
-<!-- //header -->
+	<?php include './_header.php'; ?>	<!-- header -->
 
 
-<!-- banner -->
-	<div class="banner">
-		<div class="container">
-			<div class="banner-info animated wow zoomIn" data-wow-delay=".5s">
-				
-				<div class="wmuSlider example1">
-					<div class="wmuSliderWrapper">
-						<article style="position: absolute; width: 100%; opacity: 0;"> 
-							<div class="banner-wrap">
-								<div class="banner-info1">
-                                                                                                                                                              
-									<p>T-Shirts + Formal Pants + Jewellery + Cosmetics</p>
-								</div>
-							</div>
-						</article>
-						<article style="position: absolute; width: 100%; opacity: 0;"> 
-							<div class="banner-wrap">
-								<div class="banner-info1">
-                                                                                                                                                                
-									<p>Toys + Furniture + Lighting + Watches</p>
-								</div>
-							</div>
-						</article>
-						<article style="position: absolute; width: 100%; opacity: 0;"> 
-							<div class="banner-wrap">
-								<div class="banner-info1">
-                                                                                                                                                                          
-									<p>Tops + Books & Media + Sports</p>
-								</div>
-							</div>
-						</article>
-					</div>
-				</div>
-					<script src="js/jquery.wmuSlider.js"></script> 
-					<script>
-						$('.example1').wmuSlider();         
-					</script> 
-			</div>
-		</div>
+	<div class="banner">			<!-- banner -->
 	</div>
-<!-- //banner -->
-<!-- banner-bottom -->
+
 	<div class="banner-bottom">
 		<div class="container"> 
 			<div class="banner-bottom-grids">
-				<div class="banner-bottom-grid-left animated wow slideInLeft" data-wow-delay=".5s">
-					<div class="grid">
-						<figure class="effect-julia">
-							<img src="images/4.jpg" alt=" " class="img-responsive" />
-							<figcaption>
-								<h3>Choice<span>Park</span><i> in online shopping</i></h3>
-								<div>
-									<p>Cupidatat non proident, sunt</p>
-									<p>Officia deserunt mollit anim</p>
-									<p>Laboris nisi ut aliquip consequat</p>
-								</div>
-							</figcaption>			
-						</figure>
-					</div>
+				<div class="banner-bottom-grid-left animated wow slideInLeft" data-wow-delay=".5s">	
+				<img src="images/4.jpg" height="450pxl" />
 				</div>
+
 				<div class="banner-bottom-grid-left1 animated wow slideInUp" data-wow-delay=".5s">
-					<div class="banner-bottom-grid-left-grid left1-grid grid-left-grid1">
+				<img src="images/1.jpg" alt=" " class="img-responsive" />
+				<img src="images/2.jpg" alt=" " class="img-responsive" />
+				<div class="banner-bottom-grid-left1-position">	</div>
+			</div>
+				
+			<div class="banner-bottom-grid-right animated wow slideInRight" data-wow-delay=".5s">
+					
 						<div class="banner-bottom-grid-left-grid1">
-							<img src="images/1.jpg" alt=" " class="img-responsive" />
-						</div>
-						<div class="banner-bottom-grid-left1-pos">
-							<p>Discount 45%</p>
-						</div>
-					</div>
-					<div class="banner-bottom-grid-left-grid left1-grid grid-left-grid1">
-						<div class="banner-bottom-grid-left-grid1">
-							<img src="images/2.jpg" alt=" " class="img-responsive" />
-						</div>
-						<div class="banner-bottom-grid-left1-position">
-							<div class="banner-bottom-grid-left1-pos1">
-								<p>Latest New Collections</p>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="banner-bottom-grid-right animated wow slideInRight" data-wow-delay=".5s">
-					<div class="banner-bottom-grid-left-grid grid-left-grid1">
-						<div class="banner-bottom-grid-left-grid1">
-							<img src="images/3.jpg" alt=" " class="img-responsive" />
+							<img src="images/3.jpg" height="450pxl" />
 						</div>
 						<div class="grid-left-grid1-pos">
 							<p>top and classic designs <span>2019 Collection</span></p>
 						</div>
-					</div>
+					
 				</div>
 				<div class="clearfix"> </div>
 			</div>
 		</div>
+
 	</div>
-<!-- //banner-bottom -->
-<!-- collections -->
+
+
 	<div class="new-collections">
 		<div class="container">
-			<h3 class="animated wow zoomIn" data-wow-delay=".5s">New Collections</h3>
-			
+
+			<h3 class="animated wow zoomIn" data-wow-delay=".5s"><b><font colou=blue>Latest Products</b></h3>
+			<img src="images/140.jpg" height="80pxl" width="720pxl" /></a>
 			<div class="new-collections-grids">
 				<div class="col-md-3 new-collections-grid">
 					<div class="new-collections-grid1 animated wow slideInUp" data-wow-delay=".5s">
@@ -125,162 +62,68 @@ Author: H.A.D.A. Perera
 							<div class="new-collections-grid1-image-pos">
 								<a href="single.html">Quick View</a>
 							</div>
-							<div class="new-collections-grid1-right">
-								<div class="rating">
-									<div class="rating-left">
-										<img src="images/2.png" alt=" " class="img-responsive" />
-									</div>
-									<div class="rating-left">
-										<img src="images/2.png" alt=" " class="img-responsive" />
-									</div>
-									<div class="rating-left">
-										<img src="images/2.png" alt=" " class="img-responsive" />
-									</div>
-									<div class="rating-left">
-										<img src="images/1.png" alt=" " class="img-responsive" />
-									</div>
-									<div class="rating-left">
-										<img src="images/1.png" alt=" " class="img-responsive" />
-									</div>
-									<div class="clearfix"> </div>
-								</div>
-							</div>
+							
 						</div>
-						<h4><a href="single.html">Formal Shirt</a></h4>
-						<p>Vel illum qui dolorem eum fugiat.</p>
+						<h4><a href="single.html">Party Wear</a></h4>
+						
 						
 					</div>
 					<div class="new-collections-grid1 animated wow slideInUp" data-wow-delay=".5s">
 						<div class="new-collections-grid1-image">
-							<a href="single.html" class="product-image"><img src="images/8.jpg" alt=" " class="img-responsive" /></a>
+							<a href="single.html" class="product-image"><img src="images/8.jpg" height="310pxl"/></a>
 							<div class="new-collections-grid1-image-pos">
 								<a href="single.html">Quick View</a>
 							</div>
-							<div class="new-collections-grid1-right">
-								<div class="rating">
-									<div class="rating-left">
-										<img src="images/2.png" alt=" " class="img-responsive" />
-									</div>
-									<div class="rating-left">
-										<img src="images/2.png" alt=" " class="img-responsive" />
-									</div>
-									<div class="rating-left">
-										<img src="images/1.png" alt=" " class="img-responsive" />
-									</div>
-									<div class="rating-left">
-										<img src="images/1.png" alt=" " class="img-responsive" />
-									</div>
-									<div class="rating-left">
-										<img src="images/1.png" alt=" " class="img-responsive" />
-									</div>
-									<div class="clearfix"> </div>
-								</div>
-							</div>
+							
 						</div>
-						<h4><a href="single.html">Running Shoes</a></h4>
-						<p>Vel illum qui dolorem eum fugiat.</p>
+						<h4><a href="single.html">Outdoor Slippers</a></h4>
+						
 						
 					</div>
 				</div>
 				<div class="col-md-6 new-collections-grid">
 					<div class="new-collections-grid1 new-collections-grid1-image-width animated wow slideInUp" data-wow-delay=".5s">
 						<div class="new-collections-grid1-image">
-							<a href="single.html" class="product-image"><img src="images/5.jpg" alt=" " class="img-responsive" /></a>
+							<a href="single.html" class="product-image"><img src="images/5.jpg" height="320pxl" /></a>
 							<div class="new-collections-grid1-image-pos new-collections-grid1-image-pos1">
 								<a href="single.html">Quick View</a>
 							</div>
-							<div class="new-collections-grid1-right new-collections-grid1-right-rate">
-								<div class="rating">
-									<div class="rating-left">
-										<img src="images/2.png" alt=" " class="img-responsive" />
-									</div>
-									<div class="rating-left">
-										<img src="images/2.png" alt=" " class="img-responsive" />
-									</div>
-									<div class="rating-left">
-										<img src="images/2.png" alt=" " class="img-responsive" />
-									</div>
-									<div class="rating-left">
-										<img src="images/2.png" alt=" " class="img-responsive" />
-									</div>
-									<div class="rating-left">
-										<img src="images/1.png" alt=" " class="img-responsive" />
-									</div>
-									<div class="clearfix"> </div>
-								</div>
-							</div>
+							
+							
 							<div class="new-one">
 								<p>New</p>
 							</div>
 						</div>
 						<h4><a href="single.html">Dining Table</a></h4>
-						<p>Vel illum qui dolorem eum fugiat.</p>
+						
 						
 					</div>
 					<div class="new-collections-grid-sub-grids">
 						<div class="new-collections-grid1-sub">
 							<div class="new-collections-grid1 animated wow slideInUp" data-wow-delay=".5s">
 								<div class="new-collections-grid1-image">
-									<a href="single.html" class="product-image"><img src="images/6.jpg" alt=" " class="img-responsive" /></a>
+									<a href="single.html" class="product-image"><img src="images/6.jpg" height="310pxl"  /></a>
 									<div class="new-collections-grid1-image-pos">
 										<a href="single.html">Quick View</a>
 									</div>
-									<div class="new-collections-grid1-right">
-										<div class="rating">
-											<div class="rating-left">
-												<img src="images/2.png" alt=" " class="img-responsive" />
-											</div>
-											<div class="rating-left">
-												<img src="images/2.png" alt=" " class="img-responsive" />
-											</div>
-											<div class="rating-left">
-												<img src="images/2.png" alt=" " class="img-responsive" />
-											</div>
-											<div class="rating-left">
-												<img src="images/2.png" alt=" " class="img-responsive" />
-											</div>
-											<div class="rating-left">
-												<img src="images/2.png" alt=" " class="img-responsive" />
-											</div>
-											<div class="clearfix"> </div>
-										</div>
-									</div>
+									
 								</div>
-								<h4><a href="single.html">Wall Lamp</a></h4>
-								<p>Vel illum qui dolorem eum fugiat.</p>
+								<h4><a href="single.html">Ladies Bag</a></h4>
+								
 								
 							</div>
 						</div>
 						<div class="new-collections-grid1-sub">
 							<div class="new-collections-grid1 animated wow slideInUp" data-wow-delay=".5s">
 								<div class="new-collections-grid1-image">
-									<a href="single.html" class="product-image"><img src="images/9.jpg" alt=" " class="img-responsive" /></a>
+									<a href="single.html" class="product-image"><img src="images/9.jpg"  height="310pxl" width="250pxl"/></a>
 									<div class="new-collections-grid1-image-pos">
 										<a href="single.html">Quick View</a>
 									</div>
-									<div class="new-collections-grid1-right">
-										<div class="rating">
-											<div class="rating-left">
-												<img src="images/2.png" alt=" " class="img-responsive" />
-											</div>
-											<div class="rating-left">
-												<img src="images/1.png" alt=" " class="img-responsive" />
-											</div>
-											<div class="rating-left">
-												<img src="images/1.png" alt=" " class="img-responsive" />
-											</div>
-											<div class="rating-left">
-												<img src="images/1.png" alt=" " class="img-responsive" />
-											</div>
-											<div class="rating-left">
-												<img src="images/1.png" alt=" " class="img-responsive" />
-											</div>
-											<div class="clearfix"> </div>
-										</div>
-									</div>
+									
 								</div>
 								<h4><a href="single.html">Wall Lamp</a></h4>
-								<p>Vel illum qui dolorem eum fugiat.</p>
+								
 								
 							</div>
 						</div>
@@ -290,64 +133,26 @@ Author: H.A.D.A. Perera
 				<div class="col-md-3 new-collections-grid">
 					<div class="new-collections-grid1 animated wow slideInUp" data-wow-delay=".5s">
 						<div class="new-collections-grid1-image">
-							<a href="single.html" class="product-image"><img src="images/10.jpg" alt=" " class="img-responsive" /></a>
+							<a href="single.html" class="product-image"><img src="images/10.jpg" height="310pxl" /></a>
 							<div class="new-collections-grid1-image-pos">
 								<a href="single.html">Quick View</a>
 							</div>
-							<div class="new-collections-grid1-right">
-								<div class="rating">
-									<div class="rating-left">
-										<img src="images/2.png" alt=" " class="img-responsive" />
-									</div>
-									<div class="rating-left">
-										<img src="images/2.png" alt=" " class="img-responsive" />
-									</div>
-									<div class="rating-left">
-										<img src="images/2.png" alt=" " class="img-responsive" />
-									</div>
-									<div class="rating-left">
-										<img src="images/1.png" alt=" " class="img-responsive" />
-									</div>
-									<div class="rating-left">
-										<img src="images/1.png" alt=" " class="img-responsive" />
-									</div>
-									<div class="clearfix"> </div>
-								</div>
-							</div>
+							
 						</div>
-						<h4><a href="single.html">Pearl & Stone Anklet</a></h4>
-						<p>Vel illum qui dolorem eum fugiat.</p>
+						<h4><a href="single.html">Neckless</a></h4>
+						
 						
 					</div>
 					<div class="new-collections-grid1 animated wow slideInUp" data-wow-delay=".5s">
 						<div class="new-collections-grid1-image">
-							<a href="single.html" class="product-image"><img src="images/11.jpg" alt=" " class="img-responsive" /></a>
+							<a href="single.html" class="product-image"><img src="images/11.jpg" width="250pxl" /></a>
 							<div class="new-collections-grid1-image-pos">
 								<a href="single.html">Quick View</a>
 							</div>
-							<div class="new-collections-grid1-right">
-								<div class="rating">
-									<div class="rating-left">
-										<img src="images/2.png" alt=" " class="img-responsive" />
-									</div>
-									<div class="rating-left">
-										<img src="images/2.png" alt=" " class="img-responsive" />
-									</div>
-									<div class="rating-left">
-										<img src="images/2.png" alt=" " class="img-responsive" />
-									</div>
-									<div class="rating-left">
-										<img src="images/2.png" alt=" " class="img-responsive" />
-									</div>
-									<div class="rating-left">
-										<img src="images/1.png" alt=" " class="img-responsive" />
-									</div>
-									<div class="clearfix"> </div>
-								</div>
-							</div>
+							
 						</div>
-						<h4><a href="single.html">Stones Bangles</a></h4>
-						<p>Vel illum qui dolorem eum fugiat.</p>
+						<h4><a href="single.html">Quartz Watches</a></h4>
+						
 						
 					</div>
 				</div>
@@ -355,68 +160,20 @@ Author: H.A.D.A. Perera
 			</div>
 		</div>
 	</div>
-<!-- //collections -->
-<!-- new-timer -->
-	<div class="timer">
-		<div class="container">
-			<div class="timer-grids">
-				<div class="col-md-8 timer-grid-left animated wow slideInLeft" data-wow-delay=".5s">
-					<h3><a href="products.html">sunt in culpa qui officia deserunt mollit</a></h3>
-					<div class="rating">
-						<div class="rating-left">
-							<img src="images/2.png" alt=" " class="img-responsive" />
-						</div>
-						<div class="rating-left">
-							<img src="images/2.png" alt=" " class="img-responsive" />
-						</div>
-						<div class="rating-left">
-							<img src="images/2.png" alt=" " class="img-responsive" />
-						</div>
-						<div class="rating-left">
-							<img src="images/2.png" alt=" " class="img-responsive" />
-						</div>
-						<div class="rating-left">
-							<img src="images/1.png" alt=" " class="img-responsive" />
-						</div>
-						<div class="clearfix"> </div>
-					</div>
-					<div class="new-collections-grid1-left simpleCart_shelfItem timer-grid-left-price">
-						<p><i>$580</i> <span class="item_price">$500</span></p>
-						<h4>Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam,
-							nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit 
-							qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui 
-							dolorem eum fugiat quo voluptas nulla pariatur.</h4>
-						
-					</div>
-					<div id="counter"> </div>
-					<script src="js/jquery.countdown.js"></script>
-					<script src="js/script.js"></script>
-				</div>
-				<div class="col-md-4 timer-grid-right animated wow slideInRight" data-wow-delay=".5s">
-					<div class="timer-grid-right1">
-						<img src="images/17.jpg" alt=" " class="img-responsive" />
-						<div class="timer-grid-right-pos">
-							<h4>Special Offer</h4>
-						</div>
-					</div>
-				</div>
-				<div class="clearfix"> </div>
-			</div>
-		</div>
-	</div>
-<!-- //new-timer -->
+
+
 <!-- collections-bottom -->
 	<div class="collections-bottom">
 		<div class="container">
 			<div class="collections-bottom-grids">
 				<div class="collections-bottom-grid animated wow slideInLeft" data-wow-delay=".5s">
-					<h3>45% Offer For <span>Women & Children's</span>At Christmas Season</h3>
+					<h3>25% Offer For <span><font color = yellow>Women's Footwear</font></span><font color = green>At Christmas Season</font></h3>
 				</div>
 			</div>
 			<div class="newsletter animated wow slideInUp" data-wow-delay=".5s">
-				<h2>Newsletter</h2>
+				<h1><font color = red>Newsletter</font></h1>
 				<p>Want To Read More ? Sign Up Today... </p>
-                                                                        <p><h5>It's Free !</h5></p>
+                <p><h5>It's Free !</h5></p>
 				<form>
 					<span class="glyphicon glyphicon-envelope" aria-hidden="true"></span>
 					<input type="email" value="Enter your email address" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Enter your email address';}" required="">
@@ -425,9 +182,9 @@ Author: H.A.D.A. Perera
 			</div>
 		</div>
 	</div>
-<!-- //collections-bottom -->
-<!-- footer -->
-	<?php include './_footer.php'; ?>
-<!-- //footer -->
+
+
+	<?php include './_footer.php'; ?>				<!-- footer -->
+
 </body>
 </html>
